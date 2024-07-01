dbfc75efd9c258fc2ece9a5bc6801c6bc2361eeb
Fernando Cesar da Silva <fcgpsjob@gmail.com>
2024-07-01 09:12:14 -0300

    Add tests SendUpdateVehiclesCommandVerifyEntityAndEventFail

--- tests/Rent.Vehicles.Consumers.IntegrationTests/BackgroundServices/UpdateVehiclesCommandBackgroundServiceTests.cs
+++ tests/Rent.Vehicles.Consumers.IntegrationTests/BackgroundServices/UpdateVehiclesCommandBackgroundServiceTests.cs
@@ -11,6 +11,7 @@ using Rent.Vehicles.Consumers.IntegrationTests.Extensions;
 using Rent.Vehicles.Entities;
 using Rent.Vehicles.Messages;
 using Rent.Vehicles.Messages.Commands;
+using Rent.Vehicles.Messages.Events;
 using Rent.Vehicles.Producers.Interfaces;
 using Rent.Vehicles.Services.DataServices.Interfaces;
 using Rent.Vehicles.Services.Interfaces;
@@ -88,6 +89,8 @@ public class UpdateVehiclesCommandBackgroundServiceTests
 
         await vehicleRepository.CreateAsync(@event, cancellationTokenSource.Token);
 
+        @event.LicensePlate = command.LicensePlate;
+
         var found = false;
 
         do
@@ -95,7 +98,6 @@ public class UpdateVehiclesCommandBackgroundServiceTests
             var commandResult = await commandDataService
                 .GetAsync(x => x.SagaId == command.SagaId);
 
-            @event.LicensePlate = command.LicensePlate;
             var vehicleResult = await vehicleDataService
                 .GetAsync(GetVehiclePredicate(@event));
 
@@ -110,6 +112,77 @@ public class UpdateVehiclesCommandBackgroundServiceTests
         found.Should().BeTrue();
 
         await _classFixture.GetRequiredService<UpdateVehiclesProjectionEventBackgroundService>()
+            .StopAsync(cancellationTokenSource.Token);
+
+        await _classFixture.GetRequiredService<UpdateVehiclesEventBackgroundService>()
+            .StopAsync(cancellationTokenSource.Token);
+        
+        await _classFixture.GetRequiredService<UpdateVehiclesCommandBackgroundService>()
+            .StopAsync(cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task SendUpdateVehiclesCommandVerifyEntityAndEventFail()
+    {
+        var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(60));
+
+        var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+
+        var command = _fixture
+            .Build<UpdateVehiclesCommand>()
+            .Create();
+
+        await _classFixture.GetRequiredService<IPublisher>()
+            .PublishCommandAsync(command, cancellationTokenSource.Token);
+
+        await _classFixture.GetRequiredService<UpdateVehiclesCommandBackgroundService>()
+            .StartAsync(cancellationTokenSource.Token);
+
+        await _classFixture.GetRequiredService<UpdateVehiclesEventBackgroundService>()
+            .StartAsync(cancellationTokenSource.Token);
+
+        await _classFixture.GetRequiredService<EventBackgroundService>()
+            .StartAsync(cancellationTokenSource.Token);
+
+        var commandDataService = _classFixture
+            .GetRequiredService<ICommandDataService>();
+
+        var eventDataService = _classFixture
+            .GetRequiredService<IEventDataService>();
+
+        var vehicleRepository = _classFixture
+            .GetRequiredService<IRepository<Vehicle>>();
+
+        var entity = _fixture
+            .Build<Vehicle>()
+                .With(x => x.LicensePlate, command.LicensePlate)
+                .With(x => x.IsRented, false)
+            .Create(); 
+
+        await vehicleRepository.CreateAsync(entity, cancellationTokenSource.Token);
+
+        var found = false;
+
+        do
+        {
+            var commandResult = await commandDataService
+                .GetAsync(x => x.SagaId == command.SagaId);
+
+            var eventResult = await eventDataService
+                .GetAsync(x => x.SagaId == command.SagaId && 
+                    x.StatusType == Entities.Types.StatusType.Fail &&
+                    x.Name == typeof(UpdateVehiclesEvent).ToString());
+
+            found = commandResult.IsSuccess &&
+                eventResult.IsSuccess;
+
+            await periodicTimer.WaitForNextTickAsync(cancellationTokenSource.Token);
+        } while (!found && !cancellationTokenSource.IsCancellationRequested);
+
+        // Assert
+        found.Should().BeTrue();
+
+        await _classFixture.GetRequiredService<EventBackgroundService>()
             .StopAsync(cancellationTokenSource.Token);
 
         await _classFixture.GetRequiredService<UpdateVehiclesEventBackgroundService>()
